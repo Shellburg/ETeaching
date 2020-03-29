290ca20230bbbc45f621852c7e2e8454c042155f
Shellburg <1349838860@qq.com>
2020-03-29 19:51:23 +0800

    modified:   app/src/main/java/com/sdp/eteaching/Activity/TeacherActivity/TeacherMainActivity.java 	new file:   app/src/main/java/com/sdp/eteaching/util/AudioManager.java 	new file:   app/src/main/java/com/sdp/eteaching/util/AudioRecorderButton.java 	new file:   app/src/main/java/com/sdp/eteaching/util/DialogManager.java 	new file:   app/src/main/java/com/sdp/eteaching/util/MediaManager.java 	new file:   app/src/main/java/com/sdp/eteaching/util/RecorderAdaper.java 	new file:   app/src/main/res/drawable/adj.xml 	new file:   app/src/main/res/drawable/btn_recorder_normal.xml 	new file:   app/src/main/res/drawable/btn_recording.xml 	new file:   app/src/main/res/drawable/cancel.xml 	new file:   app/src/main/res/drawable/chatto_bg_focused.xml 	new file:   app/src/main/res/drawable/dialog_loading_bg.xml 	new file:   app/src/main/res/drawable/icon.xml 	new file:   app/src/main/res/drawable/play_anim.xml 	new file:   app/src/main/res/drawable/recorder.xml 	new file:   app/src/main/res/drawable/v1.xml 	new file:   app/src/main/res/drawable/voice_to_short.xml 	new file:   app/src/main/res/layout/dialog_recorder.xml 	new file:   app/src/main/res/layout/item_recorder.xml 	modified:   app/src/main/res/layout/teacher_fragment_home.xml 	modified:   build.gradle

--- app/src/main/java/com/sdp/eteaching/Activity/TeacherActivity/TeacherMainActivity.java
+++ app/src/main/java/com/sdp/eteaching/Activity/TeacherActivity/TeacherMainActivity.java
@@ -1,11 +1,22 @@
 package com.sdp.eteaching.Activity.TeacherActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sdp.eteaching.R;
+import com.sdp.eteaching.util.AudioRecorderButton;
+import com.sdp.eteaching.util.MediaManager;
+import com.sdp.eteaching.util.RecorderAdaper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -16,6 +27,13 @@ import androidx.navigation.ui.NavigationUI;
 public class TeacherMainActivity extends AppCompatActivity {
     private String phone;
     private int teacherId;
+
+    private ListView mListView;
+    private ArrayAdapter<Recorder> mAdapter;
+    private List<Recorder> mDatas =new ArrayList<>();
+
+    private AudioRecorderButton mAudioRecorderButton;
+    private View mAnimView;
 
 
     
@@ -37,6 +55,9 @@ public class TeacherMainActivity extends AppCompatActivity {
         phone=(String)intent.getExtras().get("phone");
         teacherId=(Integer) intent.getExtras().get("t_id");
 
+
+        initView();
+        setListViewAdapter();
 
 
     }
@@ -67,5 +88,108 @@ public class TeacherMainActivity extends AppCompatActivity {
         intent.putExtras(bundle);
         startActivity(intent);
     }
+
+    private void initView(){
+        mListView = findViewById(R.id.id_listview);
+        mAudioRecorderButton = findViewById(R.id.id_recorder_button);
+
+        mAudioRecorderButton.setAudioFinishRecorderListener(new AudioRecorderButton.AudioFinishRecorderListener() {
+            @Override
+            public void onFinish(int seconds, String FilePath) {
+
+            }
+
+            @Override
+            public void onFinish(float seconds, String filePath) {
+                //每完成一次录音
+                Recorder recorder = new Recorder(seconds,filePath);
+                mDatas.add(recorder);
+                //更新adapter
+                mAdapter.notifyDataSetChanged();
+                //设置listview 位置
+                mListView.setSelection(mDatas.size()-1);
+            }
+        });
+    }
+
+    private void setListViewAdapter(){
+        mAdapter = new RecorderAdaper(this, mDatas);
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //如果第一个动画正在运行， 停止第一个播放其他的
+                if (mAnimView != null) {
+                    mAnimView.setBackgroundResource(R.drawable.adj);
+                    mAnimView = null;
+                }
+                //播放动画
+                mAnimView = view.findViewById(R.id.id_recorder_anim);
+                mAnimView.setBackgroundResource(R.drawable.play_anim);
+                AnimationDrawable animation = (AnimationDrawable) mAnimView.getBackground();
+                animation.start();
+
+                //播放音频  完成后改回原来的background
+                MediaManager.playSound(mDatas.get(position).filePath, new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mAnimView.setBackgroundResource(R.drawable.adj);
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * 根据生命周期 管理播放录音
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MediaManager.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MediaManager.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MediaManager.release();
+    }
+
+    //数据类
+    public class Recorder{
+
+        float time;
+        String filePath;
+
+        public float getTime() {
+            return time;
+        }
+
+        public void setTime(float time) {
+            this.time = time;
+        }
+
+        public String getFilePath() {
+            return filePath;
+        }
+
+        public void setFilePath(String filePath) {
+            this.filePath = filePath;
+        }
+
+        public Recorder(float time, String filePath) {
+            super();
+            this.time = time;
+            this.filePath = filePath;
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/sdp/eteaching/util/RecorderAdaper.java
@@ -0,0 +1,75 @@
+package com.sdp.eteaching.util;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.sdp.eteaching.Activity.TeacherActivity.TeacherMainActivity;
+import com.sdp.eteaching.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ *list的adapter
+ * Created by Administrator on 2017/11/28.
+ */
+
+public class RecorderAdaper extends ArrayAdapter<TeacherMainActivity.Recorder> {
+
+    //item 最小最大值
+    private int mMinItemWidth;
+    private int mMaxIItemWidth;
+
+    private LayoutInflater mInflater;
+
+    public RecorderAdaper(@NonNull Context context, List<TeacherMainActivity.Recorder> datas) {
+        super(context,-1, datas);
+        mInflater = LayoutInflater.from(context);
+
+        //获取屏幕宽度
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+
+        //item 设定最小最大值
+        mMaxIItemWidth = (int) (outMetrics.widthPixels * 0.7f);
+        mMinItemWidth = (int) (outMetrics.widthPixels * 0.15f);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.item_recorder, parent, false);
+            holder = new ViewHolder();
+            holder.seconds = convertView.findViewById(R.id.id_recorder_time);
+            holder.length = convertView.findViewById(R.id.id_recorder_length);
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        //设置时间  matt.round 四舍五入
+        holder.seconds.setText(Math.round(getItem(position).getTime())+"\"");
+        //设置背景的宽度
+        ViewGroup.LayoutParams lp = holder.length.getLayoutParams();
+        //getItem(position).time
+        lp.width = (int) (mMinItemWidth + (mMaxIItemWidth / 60f*getItem(position).getTime()));
+
+        return convertView;
+    }
+
+    private class ViewHolder{
+        TextView seconds;
+        View length;
+    }
+}

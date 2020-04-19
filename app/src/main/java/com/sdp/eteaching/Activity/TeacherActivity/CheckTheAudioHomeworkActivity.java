6c8b8c714935be3512d421cf39dcc0257d9c64c1
Shellburg <1349838860@qq.com>
2020-04-19 22:22:46 +0800

    0419

--- app/src/main/java/com/sdp/eteaching/Activity/TeacherActivity/CheckTheAudioHomeworkActivity.java
+++ app/src/main/java/com/sdp/eteaching/Activity/TeacherActivity/CheckTheAudioHomeworkActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Environment;
 
 import com.sdp.eteaching.R;
+import com.sdp.eteaching.util.FileUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -28,15 +29,16 @@ public class CheckTheAudioHomeworkActivity extends AppCompatActivity {
         homeworkPath=(String)intent.getExtras().get("path");
 
         String[] filename=homeworkPath.split("/");
-        downLoad("http://192.168.2.218:10080/uploadAudio/getfile.do",filename[3]);
+        downLoad("http://192.168.2.218:10080/uploadAudio/getfile.do?homeworkPath="+homeworkPath,filename[3]);
+        //downLoad("http://192.168.2.218:10080/uploadAudio/getfile.do?homeworkPath=E:/Eteaching/audio/hhh.txt","hhh.txt");
     }
 
     /**
      * 从服务器下载文件
      * @param path 下载文件的地址
-     * @param FileName 文件名字
+     * @param fileName 文件名字
      */
-    public static void downLoad(final String path, final String FileName) {
+    public static void downLoad(final String path, final String fileName) {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -45,28 +47,32 @@ public class CheckTheAudioHomeworkActivity extends AppCompatActivity {
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
                     con.setReadTimeout(5000);
                     con.setConnectTimeout(5000);
+                    con.setDoInput(true);
+                    con.setDoOutput(true);
                     con.setRequestProperty("Charset", "UTF-8");
                     con.setRequestMethod("GET");
+                    con.connect();
                     if (con.getResponseCode() == 200) {
                         InputStream is = con.getInputStream();//获取输入流
                         FileOutputStream fileOutputStream = null;//文件输出流
                         if (is != null) {
-                            //FileUtils fileUtils=new FileUtils();
+                            FileUtils fileUtils=new FileUtils();
                             String rootPath = Environment.getExternalStorageDirectory() + "/received_audios";
                             File dir = new File(rootPath);
                             if (!dir.exists()) {
                                 dir.mkdir();
                             }
-                            fileOutputStream = new FileOutputStream(rootPath+FileName);//指定文件保存路径，代码看下一步
+                            fileOutputStream = new FileOutputStream(fileUtils.createFile(fileName));//指定文件保存路径，代码看下一步
                             byte[] buf = new byte[1024];
                             int ch;
-                            while ((ch = is.read(buf)) != -1) {
+                            while ((ch = is.read(buf)) !=-1) {
                                 fileOutputStream.write(buf, 0, ch);//将获取到的流写入文件中
                             }
                         }
                         if (fileOutputStream != null) {
                             fileOutputStream.flush();
                             fileOutputStream.close();
+                            con.disconnect();
                         }
                     }
                 } catch (Exception e) {
